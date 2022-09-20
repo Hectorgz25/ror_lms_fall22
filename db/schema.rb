316b5ed533a5c121c661d872510397bc2050c54f
Hector Gonzalez <hectorgz25@icloud.com>
2022-09-19 20:44:40 -0600

    show and add users done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_20_002623) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_20_024323) do
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.text "desc"
+    t.string "ctype"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
