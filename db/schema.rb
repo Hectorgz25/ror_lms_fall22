6ce7c39dfad8b93400b3918c3628ea96b697c893
Hector Gonzalez <hectorgz25@icloud.com>
2022-09-20 19:26:40 -0600

    enrollment back end done

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_20_024323) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_21_004902) do
   create_table "courses", force: :cascade do |t|
     t.string "title"
     t.text "desc"
     t.string "ctype"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "enrollments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "course_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_enrollments_on_course_id"
+    t.index ["user_id"], name: "index_enrollments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,4 +36,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_20_024323) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "enrollments", "courses"
+  add_foreign_key "enrollments", "users"
 end
